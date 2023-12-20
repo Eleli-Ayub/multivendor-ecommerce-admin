1366fc3c185f4a16b7dee5cfcece439185492318
Jane49-cloud <janendirangu49@gmail.com>
2023-12-20 17:18:28 +0300

    Ads Statistics

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -27,85 +27,162 @@ const Ads = () => {
         setfilteredAds(totalAdsCount);
     }, [totalAdsCount]);
 
-    // console.log(totalAdsCount, approvedAdsCount, declinedAdsCount, closedAdsCount, pendingAdsCount);
+    const totalPercentage = (totalAdsCount?.length / ads.length) * 100;
+    const approvedPercentage = (approvedAdsCount?.length / ads.length) * 100;
+    const pendingPercentage = (pendingAdsCount?.length / ads.length) * 100;
+    const closedPercentage = (closedAdsCount?.length / ads.length) * 100;
+    const declinedPercentage = (declinedAdsCount?.length / ads.length) * 100;
 
     return (
         <div className="flex flex-col mx-auto">
             {isLoading && <Loader />}
-            <div className="bg-white shadow-card p-5">
-                <div className="flex gap-5 flex-wrap">
-                    {/* Total Ads */}
-                    <div
-                        className="p-4 rounded h-[100px] w-[220px] shadow-custom flex items-center justify-between bg-primary-orange text-white cursor-pointer"
-                        onClick={() => {
-                            setfilteredAds(totalAdsCount);
-                        }}
-                    >
-                        <LibraryAdd className="text-white" />
+            <div className="flex  gap-4 ">
+                {/* for the bars */}
+                <div className="w-1/2  h-300 bg-white radius-[8px] shadow-card ">
+                    <h1 className="text-center mt-2">Ads Statistics</h1>
+
+                    <div className="flex flex-col mx-auto gap-6 max-w-xl px-20 pb-5 mt-5">
                         <div>
-                            <h1 className="text-2xl font-semibold text-white">
-                                {totalAdsCount?.length}
-                            </h1>
-                            <p className="text-sm">Total Ads</p>
+                            <span className="text-black">Total</span>
+                            <div className="relative">
+                                <div className="w-full bg-gray-200 rounded-sm   h-[10px]">
+                                    <div
+                                        className="bg-primary-orange h-full rounded-sm"
+                                        style={{ width: `${totalPercentage}%` }}
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="text-black">Approved</span>
+                            <div className="relative">
+                                <div className="w-full bg-gray-200 rounded-sm   h-[10px]">
+                                    <div
+                                        className="bg-green-500 h-full rounded-sm"
+                                        style={{ width: `${approvedPercentage}%` }}
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="text-black">Pending</span>
+                            <div className="relative">
+                                <div className="w-full bg-gray-200 rounded-sm   h-[10px]">
+                                    <div
+                                        className="bg-orange-400  h-full rounded-sm"
+                                        style={{ width: `${pendingPercentage}%` }}
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="text-black">Declined</span>
+                            <div className="relative">
+                                <div className="w-full bg-gray-200 rounded-sm   h-[10px]">
+                                    <div
+                                        className="bg-red-600  h-full rounded-sm"
+                                        style={{ width: `${declinedPercentage}%` }}
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="text-black">Closed</span>
+                            <div className="relative">
+                                <div className="w-full bg-gray-200 rounded-sm   h-[10px]">
+                                    <div
+                                        className=" bg-green-800 h-full rounded-sm"
+                                        style={{ width: `${closedPercentage}%` }}
+                                    ></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    {/* Approved Ads */}
-                    <div
-                        className="p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-green-500 cursor-pointer"
-                        onClick={() => {
-                            setfilteredAds(approvedAdsCount);
-                        }}
-                    >
-                        <CheckOutlined className="text-whites" />
-                        <div>
-                            <h1 className="text-2xl font-semibold ">{approvedAdsCount?.length}</h1>
-                            <p className="text-sm">Approved Ads</p>
+                </div>
+                <div className="bg-white shadow-card p-5 radius-[8px]">
+                    <div className="flex gap-5 flex-wrap">
+                        {/* Total Ads */}
+                        <div
+                            className="p-4 rounded h-[100px] w-[220px]  shadow-custom flex items-center justify-between bg-primary-orange text-white cursor-pointer"
+                            onClick={() => {
+                                setfilteredAds(totalAdsCount);
+                            }}
+                        >
+                            <LibraryAdd className="text-white" />
+                            <div>
+                                <h1 className="text-2xl font-semibold text-white">
+                                    {totalAdsCount?.length}
+                                </h1>
+                                <p className="text-sm">Total Ads</p>
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Pending Ads */}
-                    <div
-                        className=" p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-orange-400 cursor-pointer"
-                        onClick={() => {
-                            setfilteredAds(pendingAdsCount);
-                        }}
-                    >
-                        <Pending />
-                        <div>
-                            {' '}
-                            <h1 className="text-2xl font-semibold ">{pendingAdsCount?.length}</h1>
-                            <p className="text-sm">Pending Ads</p>
+                        {/* Approved Ads */}
+                        <div
+                            className="p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-green-500 cursor-pointer"
+                            onClick={() => {
+                                setfilteredAds(approvedAdsCount);
+                            }}
+                        >
+                            <CheckOutlined className="text-whites" />
+                            <div>
+                                <h1 className="text-2xl font-semibold ">
+                                    {approvedAdsCount?.length}
+                                </h1>
+                                <p className="text-sm">Approved Ads</p>
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Declined Ads */}
-                    <div
-                        className=" p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-red-600 cursor-pointer"
-                        onClick={() => {
-                            setfilteredAds(declinedAdsCount);
-                        }}
-                    >
-                        <Cancel />
-                        <div>
-                            <h1 className="text-2xl font-semibold ">{declinedAdsCount?.length}</h1>
-                            <p className="text-sm">Declined Ads</p>
+                        {/* Pending Ads */}
+                        <div
+                            className=" p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-orange-400 cursor-pointer"
+                            onClick={() => {
+                                setfilteredAds(pendingAdsCount);
+                            }}
+                        >
+                            <Pending />
+                            <div>
+                                {' '}
+                                <h1 className="text-2xl font-semibold ">
+                                    {pendingAdsCount?.length}
+                                </h1>
+                                <p className="text-sm">Pending Ads</p>
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Closed Ads */}
-                    <div
-                        className="p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-green-800 cursor-pointer"
-                        onClick={() => {
-                            setfilteredAds(closedAdsCount);
-                        }}
-                    >
-                        <h1 className="text-2xl font-semibold ">{closedAdsCount?.length}</h1>
-                        <p className="text-sm">Closed Ads</p>
+                        {/* Declined Ads */}
+                        <div
+                            className=" p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-red-600 cursor-pointer"
+                            onClick={() => {
+                                setfilteredAds(declinedAdsCount);
+                            }}
+                        >
+                            <Cancel />
+                            <div>
+                                <h1 className="text-2xl font-semibold ">
+                                    {declinedAdsCount?.length}
+                                </h1>
+                                <p className="text-sm">Declined Ads</p>
+                            </div>
+                        </div>
+
+                        {/* Closed Ads */}
+                        <div
+                            className="p-4 rounded h-[100px] w-[220px] shadow-custom  flex items-center justify-between text-white bg-green-800 cursor-pointer"
+                            onClick={() => {
+                                setfilteredAds(closedAdsCount);
+                            }}
+                        >
+                            <h1 className="text-2xl font-semibold ">{closedAdsCount?.length}</h1>
+                            <p className="text-sm">Closed Ads</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
             {/* second placeholder */}
             <div>
                 <div>
