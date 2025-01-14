034f80732c3171c71bcc57464e5211d742793b09
Jane49-cloud <janendirangu49@gmail.com>
2025-01-15 01:26:37 +0300

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes/Index';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes/Index";
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter basename={'/admin'}>
-                {/* <BrowserRouter> */}
-                <Routes />
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      {/* <BrowserRouter basename={'/admin'}> */}
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
