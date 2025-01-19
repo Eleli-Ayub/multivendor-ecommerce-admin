1781e8a75b6dba67d26d0c3899af09ad54f8cd61
Jane49-cloud <janendirangu49@gmail.com>
2025-01-19 15:05:03 +0300

    updated baseurl

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,32 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosService = axios.create({
-    baseURL: 'https://api.eduka.co.ke/',
-    // baseURL: 'http://localhost:8000',
-    // baseURL: 'http://192.168.0.112:8000',
-    // baseURL: 'http://137.184.189.199:8000',
+  baseURL: "https://ecommerce.eleliafrika.cloud",
+  // baseURL: 'http://localhost:8000',
+  // baseURL: 'http://192.168.0.112:8000',
+  // baseURL: 'http://137.184.189.199:8000',
 });
 
 // Add a response interceptor to handle errors
 axiosService.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response && error.response.status === 400) {
-            // Log the error response body
-            console.error('Error Response Body:', error.response.data);
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 400) {
+      // Log the error response body
+      console.error("Error Response Body:", error.response.data);
     }
+    return Promise.reject(error);
+  }
 );
 
 // Add a request interceptor
 axiosService.interceptors.request.use(async (req) => {
-    let token = localStorage.getItem('adminToken');
-    if (token) {
-        req.headers['x-access-token'] = token;
-    }
+  let token = localStorage.getItem("adminToken");
+  if (token) {
+    req.headers["x-access-token"] = token;
+  }
 
-    return req;
+  return req;
 });
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,108 +1,108 @@
-import React, { useState } from 'react';
-import Logo from '../assets/logo.jpeg';
-import { useDispatch, useSelector } from 'react-redux';
-import { FaEyeSlash, FaEye } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import { LoggingUser } from '../Redux/slices/AuthSlice';
-import { AppDispatch } from '../Redux/store';
-import Loader from './constants/loader';
+import React, { useState } from "react";
+import Logo from "../assets/logo.jpeg";
+import { useDispatch, useSelector } from "react-redux";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { LoggingUser } from "../Redux/slices/AuthSlice";
+import { AppDispatch } from "../Redux/store";
+import Loader from "./constants/loader";
 
 const LoginForm: React.FC = ({}) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch<AppDispatch>();
-    const [showPassword, setShowPassword] = useState(false);
-    // const isLoading = useSelector((state: any) => state.auth.isLoading);
-    const { isLoading } = useSelector((state: any) => ({ ...state.auth }));
-    // const token = useSelector((state: any) => state.auth.userToken);
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const [showPassword, setShowPassword] = useState(false);
+  // const isLoading = useSelector((state: any) => state.auth.isLoading);
+  const { isLoading } = useSelector((state: any) => ({ ...state.auth }));
+  // const token = useSelector((state: any) => state.auth.userToken);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        await dispatch(LoggingUser(formData));
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await dispatch(LoggingUser(formData));
 
-        setTimeout(() => {
-            navigate('/');
-        }, 1000);
-    };
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
 
-    if (isLoading) {
-        return <Loader />;
-    }
+  if (isLoading) {
+    return <Loader />;
+  }
 
-    return (
-        <>
-            <div className="h-screen mx-auto p-10 bg-gray-light w-screen">
-                <div
-                    className="lg:w-2/6 h-5/6 w-full bg-white rounded-2xl p-10 shadow-2xl "
-                    style={{ margin: 'auto' }}
+  return (
+    <>
+      <div className="h-screen mx-auto p-10 bg-gray-light w-screen">
+        <div
+          className="lg:w-2/6 h-5/6 w-full bg-white rounded-2xl p-10 shadow-2xl "
+          style={{ margin: "auto" }}
+        >
+          <div className="flex items-center justify-center gap-3">
+            <img src={Logo} alt="logo" className="h-24 object-cover " />
+          </div>
+          <p className="text-center">Eduka Admin</p>
+
+          <form
+            onSubmit={handleSubmit}
+            className="mx-auto p-4 border rounded-lg shadow-lg mt-4 price"
+          >
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-primary-orange h-12"
+                placeholder="Enter your Email Address"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-bold mb-2 relative"
+              >
+                Password:
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-primary-orange h-12"
+                  placeholder={`Enter password `}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute top-1/2 right-4"
                 >
-                    <div className="flex items-center justify-center gap-3">
-                        <img src={Logo} alt="logo" className="h-24 object-cover " />
-                    </div>
-                    <p className="text-center">Eduka Admin</p>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </label>
+            </div>
 
-                    <form
-                        onSubmit={handleSubmit}
-                        className="mx-auto p-4 border rounded-lg shadow-lg mt-4 price"
-                    >
-                        <div className="mb-4">
-                            <label
-                                htmlFor="email"
-                                className="block text-gray-700 text-sm font-bold mb-2"
-                            >
-                                Email:
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-primary-orange h-12"
-                                placeholder="Enter your Email Address"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label
-                                htmlFor="password"
-                                className="block text-gray-700 text-sm font-bold mb-2 relative"
-                            >
-                                Password:
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    id="password"
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-primary-orange h-12"
-                                    placeholder={`Enter password `}
-                                />
-                                <button
-                                    type="button"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                    className="absolute top-1/2 right-4"
-                                >
-                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                                </button>
-                            </label>
-                        </div>
+            <button
+              type="submit"
+              className="bg-primary-orange text-white py-2 px-4 rounded-xl hover:bg-secondary-orange transition duration-300 w-full"
+            >
+              Submit
+            </button>
 
-                        <button
-                            type="submit"
-                            className="bg-primary-orange text-white py-2 px-4 rounded-xl hover:bg-secondary-orange transition duration-300 w-full"
-                        >
-                            Submit
-                        </button>
-
-                        {/* <p className="text-gray-500 text-center mt-3">
+            {/* <p className="text-gray-500 text-center mt-3">
                             Forgot Password?
                             <a href="/login" className="ml-2 text-blue-500">
                                 Reset Password
@@ -114,11 +114,11 @@ const LoginForm: React.FC = ({}) => {
                                 Sign Up
                             </a>{' '}
                         </p> */}
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginForm;
